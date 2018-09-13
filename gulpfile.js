92c30da3c5ca7e77e90a9cf744b8020af2660f23
Alexander Ivanov <oshli.a.er@gmail.com>
2018-09-13 09:57:52 +0300

    add envs

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+/* global require */
+
+/**
+ * @file gulpfile
+ * @author Alexander Ivanov <oshli.a.er@gmail.com>
+ * @see https://github.com/jsdoc2md/gulp-jsdoc-to-markdown
+ */
+
+var gulp = require('gulp');
+var gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
+var concat = require('gulp-concat');
+var log = require('fancy-log');
+
+var src = './*.gs';
+var jsdoc2mdConfig = undefined;
+
+// makedocs task
+gulp.task('makedocs', function() {
+  return gulp
+    .src(src)
+    .pipe(concat('API.md'))
+    .pipe(gulpJsdoc2md(jsdoc2mdConfig))
+    .on('error', function(err) {
+      log('jsdoc2md failed:', err.message);
+    })
+    .pipe(gulp.dest('./'));
+});
+
+// makedocs:auto task
+gulp.task('makedocs:auto', function() {
+  gulp.watch('./*.gs', ['makedocs']);
+});
